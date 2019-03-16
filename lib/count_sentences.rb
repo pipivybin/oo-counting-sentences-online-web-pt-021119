89e630797e52c7b8fa4fce27603ac6c1e63245e3
Ming Duan <11203duan@gmail.com>
2019-03-16 21:28:51 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    arr = self.split(/[.?!]/)
+    arr.delete_if {|x| x.empty?}
+    arr.length
   end
 end
